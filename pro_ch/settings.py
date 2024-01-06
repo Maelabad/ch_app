98bb66fb50da08bcd65c101b75d512ea4a062f28
Adjobo Elie <120084291+Maelabad@users.noreply.github.com>
2024-01-06 23:16:05 +0000

    Update settings.py

--- pro_ch/settings.py
+++ pro_ch/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&aof_5l-qs=df&9y3yu!mcvx$dgh=@ltxv&fd*w*eci%ew11ve'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
